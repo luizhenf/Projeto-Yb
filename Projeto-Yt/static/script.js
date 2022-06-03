4bab8c484ebb376253d3f3c618ec17a2bc5fcb44
luizhenf <luizhenf40@gmail.com>
2022-06-02 21:39:01 -0300

    animacoes

--- Projeto-Yt/static/script.js
+++ Projeto-Yt/static/script.js
@@ -22,3 +22,25 @@ navItem.forEach(item => {
         }
     })
 })
+
+// Animar todos os itens da tela que tiverem meu atributo data-anime
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+
+    item.forEach((element) => {
+        if (windowTop > element.offsetTop) {
+            element.classList.add("animate");
+        } else{
+            element.classList.remove("animate");
+        }
+    });
+};
+
+animeScroll();
+
+window.addEventListener("scroll", () =>{
+    animeScroll();
+})
